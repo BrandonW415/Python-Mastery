db52d703bba73d2cfc49650b60bc355164ea1f99
Brandon Williams <bwilliams0415@gmail.com>
2024-10-14 21:30:33 -0500

    Small Quiz Game Project

---
+++ quiz_game.py
@@ -0,0 +1,17 @@
+questions = [
+    {
+        "question": "What is the video game that won Game of the Year in 2015",
+        "options": ["Fallout 4", "Super Mario Maker", "The Witcher 3: Witch Hunt", "Bloodborne"],
+        "correct_answer": "The Witcher 3: Witch Hunt"
+    },
+    {
+        "question": "On the periodic table, what does the element S represent?",
+        "options": ["Sulfur", "Samarium", "Selenium", "Scandium"],
+        "correct_answer": "Mars"
+    },
+    {
+        "question": "Other than Steph Curry, who has made the most 3-pointers of all time in the NBA?",
+        "options": ["Damien Lillard", "James Harden", "Reggie Miller", "Ray Allen"],
+        "correct_answer": "Ray Allen"
+    }
+]
